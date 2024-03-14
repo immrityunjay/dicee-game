c591b500e3f64710e6bca2608a17c753f4c83245
immrityunjay <142257155+immrityunjay@users.noreply.github.com>
2024-03-14 19:54:47 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1 = Math.random();
 randomNumber1 = randomNumber1*6 ;
 randomNumber1 = Math.floor(randomNumber1)+1 ;
 var randomDiceImage = "dice"+randomNumber1+".png";
-var randomImageSource = "images/"+ randomDiceImage;
+var randomImageSource = "./images/"+ randomDiceImage;
 
 document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
 
@@ -11,7 +11,8 @@ randomNumber2 = randomNumber2*6 ;
 randomNumber2 = Math.floor(randomNumber2)+1 ;
 
 var randomDiceImage2 = "dice"+randomNumber2+".png";
-var randomImageSource2 = "images/"+ randomDiceImage2 ;
+var randomImageSource2 = "./images/"+ randomDiceImage2 ;
+
 
  document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
